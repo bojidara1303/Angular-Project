3c89fde093c98479c3e643424f26e3fbfb4d30b9
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-09-19 13:39:19 +0300

    Add Login component

--- movie-list/src/app/app.module.ts
+++ movie-list/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
