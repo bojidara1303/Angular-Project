006c37d123147f9b5435eb3e6c1b5210bbd4c2c4
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-09-19 16:39:22 +0300

    Add add-movie component

--- movie-list/src/app/app.module.ts
+++ movie-list/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AllMoviesComponent } from './movies/all-movies/all-movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { EditMovieComponent } from './movies/edit-movie/edit-movie.component';
+import { AddMovieComponent } from './movies/add-movie/add-movie.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,16 @@ import { LoginComponent } from './login/login.component';
     HeaderComponent,
     FooterComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    AllMoviesComponent,
+    MovieComponent,
+    AddMovieComponent,
+    EditMovieComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
