b0acfb7d12eee86204654039b298a08ca1abec72
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-09-29 20:29:36 +0300

    Creating environment

---
+++ movie-list/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    baseUrl: 'http://localhost:3030',
+    usersUrl: 'http://localhost:3030/users',
+    moviesUrl: 'http://localhost:3030/data/movies',
+    quotesUrl: 'http://localhost:3030/data/quotes',
+    newsUrl: 'http://localhost:3030/data/news',
+};
