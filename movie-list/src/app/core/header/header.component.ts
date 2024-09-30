84fda200f7452cc0895e5a2a5ecd9d900802f51d
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-09-30 21:15:06 +0300

    Edit Navigation

--- movie-list/src/app/core/header/header.component.ts
+++ movie-list/src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
+
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  get username(): string {
+    return this.userService.user?.username || '';
+  }
+  
+  logout() {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/auth/login']);
+      },
+      error: () => {
+        this.router.navigate(['/auth/login']);
+      },
+    });
+  }
 }
