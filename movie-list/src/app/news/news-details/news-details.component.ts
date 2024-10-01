553129332f4695569e7e85dfaa5ddae9a875ae8a
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 21:35:38 +0300

    Update news-details component
    Get data dynamically

--- movie-list/src/app/news/news-details/news-details.component.ts
+++ movie-list/src/app/news/news-details/news-details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
+import { News } from 'src/app/types/news';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-news-details',
   templateUrl: './news-details.component.html',
   styleUrls: ['./news-details.component.css']
 })
-export class NewsDetailsComponent {
+export class NewsDetailsComponent implements OnInit {
+  news = {} as News;
 
+  constructor(private newsService: NewsService, private activeRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((data) => {
+      const id = data['newsId'];
+
+      this.newsService.getSingleNews(id).subscribe((news) => {
+        this.news = news;
+      })
+    })
+  }
 }
