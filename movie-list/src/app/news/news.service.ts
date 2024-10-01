e06d8d009ad7a0ded3fbddd8abfff15cfaeb00fc
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:31:59 +0300

    Update news.service.ts

--- movie-list/src/app/news/news.service.ts
+++ movie-list/src/app/news/news.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { News } from '../types/news';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllNews() {
+    const { newsUrl } = environment;
+    return this.http.get<News[]>(`${newsUrl}`);
+  }
+
+  getSingleNews(id: string) {
+    const { newsUrl } = environment;
+    return this.http.get<News>(`${newsUrl}/${id}`)
+  }
 }
