a86d302f5ffbaffe6a84482f087e8f4fa2c4fe46
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 21:35:51 +0300

    Update news.routing

--- movie-list/src/app/news/news.routing.module.ts
+++ movie-list/src/app/news/news.routing.module.ts
@@ -7,7 +7,7 @@ import { NewsDetailsComponent } from './news-details/news-details.component';
 
 const routes: Routes = [
     { path: 'news', component: AllNewsComponent },
-    { path: 'news/newsId/details', component: NewsDetailsComponent }
+    { path: 'news/:newsId/details', component: NewsDetailsComponent }
 ];
 
 @NgModule({
