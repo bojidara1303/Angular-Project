785e925c939d077e910aa5d21e07eb699e3b6d06
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:12:50 +0300

    Update all-quotes component

--- movie-list/src/app/quotes/all-quotes/all-quotes.component.ts
+++ movie-list/src/app/quotes/all-quotes/all-quotes.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/types/quote';
+import { QuotesService } from '../quotes.service';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-all-quotes',
   templateUrl: './all-quotes.component.html',
   styleUrls: ['./all-quotes.component.css']
 })
-export class AllQuotesComponent {
+export class AllQuotesComponent implements OnInit {
+  quotes: Quote[] | null = [];
 
+  constructor(private quoteService: QuotesService, private userService: UserService) { }
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.quoteService.getAllQuotes().subscribe({
+      next: (quotes) => {
+        this.quotes = quotes;
+      }
+    })
+  }
 }
