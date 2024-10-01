b397bc2c2d66a8661f3e660e03ff06896c9c7fec
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:05:55 +0300

    Remove Quote and Movie components

--- movie-list/src/app/app.module.ts
+++ movie-list/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllMoviesComponent } from './movies/all-movies/all-movies.component';
-import { MovieComponent } from './movies/movie/movie.component';
 import { EditMovieComponent } from './movies/edit-movie/edit-movie.component';
 import { AddMovieComponent } from './movies/add-movie/add-movie.component';
 import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 import { AddQuoteComponent } from './quotes/add-quote/add-quote.component';
 import { AllQuotesComponent } from './quotes/all-quotes/all-quotes.component';
 import { EditQuoteComponent } from './quotes/edit-quote/edit-quote.component';
-import { QuoteComponent } from './quotes/quote/quote.component';
 import { AllNewsComponent } from './news/all-news/all-news.component';
 import { NewsDetailsComponent } from './news/news-details/news-details.component';
 import { AllReviewsComponent } from './reviews/all-reviews/all-reviews.component';
@@ -30,8 +29,9 @@ import { MoviesRoutingModule } from './movies/movies-routing.module';
 import { QuotesRoutingModule } from './quotes/quotes-routing.module';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { NewsRoutingModule } from './news/news.routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmailDirective } from './shared/validators/email.directive';
+
 
 @NgModule({
   declarations: [
@@ -41,14 +41,12 @@ import { EmailDirective } from './shared/validators/email.directive';
     RegisterComponent,
     LoginComponent,
     AllMoviesComponent,
-    MovieComponent,
     AddMovieComponent,
     EditMovieComponent,
     MovieDetailsComponent,
     AddQuoteComponent,
     AllQuotesComponent,
     EditQuoteComponent,
-    QuoteComponent,
     NewsDetailsComponent,
     AllNewsComponent,
     AllReviewsComponent,
@@ -58,7 +56,7 @@ import { EmailDirective } from './shared/validators/email.directive';
     ErrorPageComponent,
     ContactUsComponent,
     AboutUsComponent,
-    EmailDirective
+    EmailDirective,
   ],
   imports: [
     BrowserModule,
@@ -68,7 +66,9 @@ import { EmailDirective } from './shared/validators/email.directive';
     QuotesRoutingModule,
     NewsRoutingModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
