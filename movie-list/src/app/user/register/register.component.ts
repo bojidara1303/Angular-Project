1102736ed2a94c5e01e4a2bd59ac9daf0eed5a68
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:09:25 +0300

    Update Register form and validations

--- movie-list/src/app/user/register/register.component.ts
+++ movie-list/src/app/user/register/register.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
-})
+}) 
 export class RegisterComponent {
-  formSubmitHandler(form: NgForm) {
-    console.log(form);
-    
-      }
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required,Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const {
+      username,
+      email,
+      password,
+      confirmPassword
+    } = this.form.value;
+
+    this.userService
+      .register(username!, email!, password!, confirmPassword!)
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
+  }
+
 }
