cf80a30b29d78d36e87ca676c263054cc584908d
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:05:06 +0300

    Update the form and the validations

--- movie-list/src/app/movies/add-movie/add-movie.component.ts
+++ movie-list/src/app/movies/add-movie/add-movie.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MovieService } from '../movies.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-movie',
@@ -8,7 +11,36 @@ import { NgForm } from '@angular/forms';
 })
 export class AddMovieComponent {
 
-  formSubmitHandler(form: NgForm) {
-    console.log(form);
+  form = this.fb.group({
+    cover: ['', [Validators.required]],
+    title: ['', [Validators.required]],
+    genre: ['', [Validators.required]],
+    year: ['', [Validators.required, Validators.min(1900), Validators.max(2025)]],
+    cast: ['', [Validators.required]],
+    director: ['', [Validators.required]],
+    duration: ['', [Validators.required, Validators.min(0)]],
+    summary: ['', [Validators.required]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private movieService: MovieService,
+    private router: Router
+  ) { }
+
+  createMovie(): void {
+    if (this.form.invalid) {
+      return
+    }
+
+    const { 
+      cover, title, genre, year, cast, director, duration, summary
+    } = this.form.value;
+
+    this.movieService
+    .createMovie(cover!, title!, genre!, +year!, cast!, director!, +duration!, summary!)
+    .subscribe(()=>{
+      this.router.navigate(['/movies'])
+    })
   }
 }
