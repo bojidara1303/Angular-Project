12c17192a23055917582fb97287f65cb796ceb62
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:03:29 +0300

    Removing Quote components from the module

--- movie-list/src/app/quotes/quotes.module.ts
+++ movie-list/src/app/quotes/quotes.module.ts
@@ -3,29 +3,25 @@ import { CommonModule } from '@angular/common';
 import { AddQuoteComponent } from './add-quote/add-quote.component';
 import { AllQuotesComponent } from './all-quotes/all-quotes.component';
 import { EditQuoteComponent } from './edit-quote/edit-quote.component';
-import { QuoteComponent } from './quote/quote.component';
 import { RouterModule } from '@angular/router';
 import { QuotesRoutingModule } from './quotes-routing.module';
-
 
 
 @NgModule({
   declarations: [
     AddQuoteComponent,
     AllQuotesComponent,
-    EditQuoteComponent,
-    QuoteComponent
+    EditQuoteComponent
   ],
   imports: [
     CommonModule,
     QuotesRoutingModule,
-    RouterModule
+    RouterModule,
   ],
   exports:[
     AddQuoteComponent,
     AllQuotesComponent,
-    EditQuoteComponent,
-    QuoteComponent
+    EditQuoteComponent
   ]
 })
 export class QuotesModule { }
