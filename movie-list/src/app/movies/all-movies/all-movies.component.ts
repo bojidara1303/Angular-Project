72587c38cad61ece41b76c0b36ad142f1074acf9
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:07:10 +0300

    Updating CSS  of all-movies component

--- movie-list/src/app/movies/all-movies/all-movies.component.ts
+++ movie-list/src/app/movies/all-movies/all-movies.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/types/movie';
+import { MovieService } from '../movies.service';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-all-movies',
   templateUrl: './all-movies.component.html',
   styleUrls: ['./all-movies.component.css']
 })
-export class AllMoviesComponent {
+export class AllMoviesComponent implements OnInit {
+  movies: Movie[] | null = [];
+
+  constructor(private movieService: MovieService, private userService: UserService) { }
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.movieService.getAllMovies().subscribe({
+      next: (movies) => {
+        this.movies = movies;
+      }
+    })
+  }
 
 }
