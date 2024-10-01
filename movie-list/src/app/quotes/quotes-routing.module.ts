ccefcae816d5b9023988edef1d644e078dee25e6
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:08:31 +0300

    Remove Quote component from the routing module

--- movie-list/src/app/quotes/quotes-routing.module.ts
+++ movie-list/src/app/quotes/quotes-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddQuoteComponent } from './add-quote/add-quote.component';
 import { AllQuotesComponent } from './all-quotes/all-quotes.component';
 import { EditQuoteComponent } from './edit-quote/edit-quote.component';
-import { QuoteComponent } from './quote/quote.component';
 
 
 const routes: Routes = [
-    { path: 'add-quote', component: AddQuoteComponent },
+    { path: 'quotes/add-quote', component: AddQuoteComponent },
     { path: 'quotes', component: AllQuotesComponent },
     { path: 'quotes/quoteId/edit', component: EditQuoteComponent },
-    { path: 'quotes/quote', component: QuoteComponent },
 ];
 
 @NgModule({
--- movie-list/src/app/quotes/quotes.service.ts
+++ movie-list/src/app/quotes/quotes.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Quote } from '../types/quote';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuotesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllQuotes() {
+    const { quotesUrl } = environment;
+    return this.http.get<Quote[]>(`${quotesUrl}`);
+  }
+
+  getSingleQuote(id: string) {
+    const { quotesUrl } = environment;
+    return this.http.get<Quote>(`${quotesUrl}/${id}`);
+  }
+
+  createQuote(
+    imageUrl: string,
+    quote: string,
+    movie: string
+  ) {
+    return this.http
+      .post<Quote>(environment.quotesUrl, {
+        imageUrl,
+        quote,
+        movie
+      })
+  }
+
+  editQuote(imageUrl: string, quote: string, movie: string, quoteId: string) {
+    return this.http.put<Quote>(`${environment.quotesUrl}/${quoteId}`, {
+      imageUrl, quote, movie
+    })
+  }
+
+  deleteQuote(quoteId:string){
+    return this.http.delete<Quote>(`${environment.quotesUrl}/${quoteId}`)
+  }
 }
+
