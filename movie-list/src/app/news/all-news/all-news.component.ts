c8c26869612a7e5f93817068a8fa85c4bb99cc03
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 20:30:14 +0300

    Update all-news component

--- movie-list/src/app/news/all-news/all-news.component.ts
+++ movie-list/src/app/news/all-news/all-news.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { News } from 'src/app/types/news';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-all-news',
   templateUrl: './all-news.component.html',
   styleUrls: ['./all-news.component.css']
 })
-export class AllNewsComponent {
+export class AllNewsComponent implements OnInit {
+  allNews: News[] | null = [];
 
+  constructor(private newsService: NewsService) { }
+
+  ngOnInit(): void {
+    this.newsService.getAllNews().subscribe({
+      next: (allNews) => {
+        this.allNews = allNews;
+      }
+    })
+  }
 }
