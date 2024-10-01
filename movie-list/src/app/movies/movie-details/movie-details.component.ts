2a1d8301362559e7845629eead1daee37419f455
Bozhidara Tsanova <bojidara1303@gmail.com>
2024-10-01 21:10:56 +0300

    Update movie-details component

--- movie-list/src/app/movies/movie-details/movie-details.component.ts
+++ movie-list/src/app/movies/movie-details/movie-details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movies.service';
+import { Movie } from 'src/app/types/movie';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  movie = {} as Movie;
 
+  constructor(private movieService: MovieService, private activeRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((data) => {
+      const id = data['movieId'];
+
+      this.movieService.getSingleMovie(id).subscribe((movie) => {
+        this.movie = movie;
+      })
+    })
+  }
 }
